0f3bab28745807d0bbc171e44d9ecb3e610ee9ce
Brent Simmons <brent@ranchero.com>
2017-04-17 21:44:03 -0700

    Add stubs for WebserverVerbs.

--- FrontierVerbs/FrontierVerbs/VerbTables/WebserverVerbs.swift
+++ FrontierVerbs/FrontierVerbs/VerbTables/WebserverVerbs.swift
@@ -11,7 +11,13 @@ import Foundation
 struct WebserverVerbs: VerbTable {
 	
 	private enum Verb: String {
-		case x = "x"
+		case server = "server"
+		case dispatch = "dispatch"
+		case parseHeaders = "parseheaders"
+		case parseCookies = "parsecookies"
+		case buildResponse = "buildresponse"
+		case buildErrorPage = "builderrorpage"
+		case getServerString = "getserverstring"
 	}
 	
 	static func evaluate(_ lowerVerbName: String, _ params: VerbParams) -> VerbResult {
@@ -22,17 +28,60 @@ struct WebserverVerbs: VerbTable {
 		
 		switch verb {
 			
-		case .x:
-			return x(params)
+		case .server:
+			return server(params)
+		case .dispatch:
+			return dispatch(params)
+		case .parseHeaders:
+			return parseHeaders(params)
+		case .parseCookies:
+			return parseCookies(params)
+		case .buildResponse:
+			return buildResponse(params)
+		case .buildErrorPage:
+			return buildErrorPage(params)
+		case .getServerString:
+			return getServerString(params)
 		}
 	}
 }
 
 private extension WebserverVerbs {
 	
-	static func x(_ params: VerbParams) -> VerbResult {
+	static func server(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+
+	static func dispatch(_ params: VerbParams) -> VerbResult {
 		
 		return VerbResult.notImplemented
 	}
 	
+	static func parseHeaders(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func parseCookies(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func buildResponse(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func buildErrorPage(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func getServerString(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+
 }
