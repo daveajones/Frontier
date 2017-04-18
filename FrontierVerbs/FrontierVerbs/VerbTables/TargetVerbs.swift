930bc5545d437e61cd55a0329dffaa33fed93ae0
Brent Simmons <brent@ranchero.com>
2017-04-17 21:54:34 -0700

    Add stubs for TargetVerbs.

--- FrontierVerbs/FrontierVerbs/VerbTables/TargetVerbs.swift
+++ FrontierVerbs/FrontierVerbs/VerbTables/TargetVerbs.swift
@@ -11,7 +11,9 @@ import Foundation
 struct TargetVerbs: VerbTable {
 	
 	private enum Verb: String {
-		case x = "x"
+		case get = "get"
+		case set = "set"
+		case clear = "clear"
 	}
 	
 	static func evaluate(_ lowerVerbName: String, _ params: VerbParams) -> VerbResult {
@@ -22,15 +24,29 @@ struct TargetVerbs: VerbTable {
 		
 		switch verb {
 			
-		case .x:
-			return x(params)
+		case .get:
+			return get(params)
+		case .set:
+			return set(params)
+		case .clear:
+			return clear(params)
 		}
 	}
 }
 
 private extension TargetVerbs {
 	
-	static func x(_ params: VerbParams) -> VerbResult {
+	static func get(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func set(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func clear(_ params: VerbParams) -> VerbResult {
 		
 		return VerbResult.notImplemented
 	}
