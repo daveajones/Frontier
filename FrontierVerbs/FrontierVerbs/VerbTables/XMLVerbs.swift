65aed3a8f406be2726e2c8ee48876dceb8d68cad
Brent Simmons <brent@ranchero.com>
2017-04-17 21:38:41 -0700

    Add stubs for XML verbs.

--- FrontierVerbs/FrontierVerbs/VerbTables/XMLVerbs.swift
+++ FrontierVerbs/FrontierVerbs/VerbTables/XMLVerbs.swift
@@ -11,7 +11,20 @@ import Foundation
 struct XMLVerbs: VerbTable {
 	
 	private enum Verb: String {
-		case x = "x"
+		case addTable = "addtable"
+		case addValue = "addValue"
+		case compile = "compile"
+		case deCompile = "decompile"
+		case getAddress = "getaddress"
+		case getAddressList = "getaddresslist"
+		case getAttribute = "getattribute"
+		case getAttributeValue = "getattributevalue"
+		case getValue = "getvalue"
+		case valToString = "valtostring"
+		case frontierValueToTaggedText = "frontiervaluetotaggedtext"
+		case structToFrontierValue = "structtofrontiervalue"
+		case getPathAddress = "getpathaddress"
+		case convertToDisplayName = "convertToDisplayName"
 	}
 	
 	static func evaluate(_ lowerVerbName: String, _ params: VerbParams) -> VerbResult {
@@ -22,17 +35,109 @@ struct XMLVerbs: VerbTable {
 		
 		switch verb {
 			
-		case .x:
-			return x(params)
+		case .addTable:
+			return addTable(params)
+		case .addValue:
+			return addValue(params)
+		case .compile:
+			return compile(params)
+		case .deCompile:
+			return deCompile(params)
+		case .getAddress:
+			return getAddress(params)
+		case .getAddressList:
+			return getAddressList(params)
+		case .getAttribute:
+			return getAttribute(params)
+		case .getAttributeValue:
+			return getAttributeValue(params)
+		case .getValue:
+			return getValue(params)
+		case .valToString:
+			return valToString(params)
+		case .frontierValueToTaggedText:
+			return frontierValueToTaggedText(params)
+		case .structToFrontierValue:
+			return structToFrontierValue(params)
+		case .getPathAddress:
+			return getPathAddress(params)
+		case .convertToDisplayName:
+			return convertToDisplayName(params)
 		}
 	}
 }
 
 private extension XMLVerbs {
 	
-	static func x(_ params: VerbParams) -> VerbResult {
+	static func addTable(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+
+	static func addValue(_ params: VerbParams) -> VerbResult {
 		
 		return VerbResult.notImplemented
 	}
 	
+	static func compile(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func deCompile(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func getAddress(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func getAddressList(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func getAttribute(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func getAttributeValue(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func getValue(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func valToString(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func frontierValueToTaggedText(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func structToFrontierValue(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func getPathAddress(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+	static func convertToDisplayName(_ params: VerbParams) -> VerbResult {
+		
+		return VerbResult.notImplemented
+	}
+	
+
 }
