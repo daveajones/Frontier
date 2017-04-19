671b856ce006eea54b576b43e07ddc113eb6b12e
Brent Simmons <brent@ranchero.com>
2017-04-19 13:19:13 -0700

    Start ShellWindow UI.

---
+++ Frontier/ShellWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  ShellWindowController.swift
+//  Frontier
+//
+//  Created by Brent Simmons on 4/19/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Cocoa
+
+class ShellWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
