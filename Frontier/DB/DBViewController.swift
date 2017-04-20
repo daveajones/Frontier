2688be57bca8f5ee86b7631f6a247c92c7291d9b
Brent Simmons <brent@ranchero.com>
2017-04-19 19:16:36 -0700

    Start DBViewController.

---
+++ Frontier/DB/DBViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DBViewController.swift
+//  
+//
+//  Created by Brent Simmons on 4/19/17.
+//
+//
+
+import Cocoa
+
+class DBViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
