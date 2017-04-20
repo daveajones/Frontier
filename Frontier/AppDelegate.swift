dcfeccb4a15731cc35995f28fa7a88847255f56c
Brent Simmons <brent@ranchero.com>
2017-04-19 21:24:23 -0700

    Add Preferences window, controller, and initial preference. Register the default for that initial preference.

--- Frontier/AppDelegate.swift
+++ Frontier/AppDelegate.swift
@@ -11,16 +11,35 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+	var preferencesWindowController: NSWindowController?
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+
+		let defaults: [String: Any] = [DefaultsKey.launchOpenODB: true]
+		UserDefaults.standard.register(defaults: defaults)
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
+
 	}
 
-
+	// MARK: Actions
+	
+	@IBAction func showPreferences(_ sender: AnyObject) {
+		
+		if preferencesWindowController == nil {
+			preferencesWindowController = windowController(withStoryboardName: "Preferences")
+		}
+		preferencesWindowController!.showWindow(self)
+	}
 }
 
+private extension AppDelegate {
+	
+	func windowController(withStoryboardName name: String) -> NSWindowController {
+		
+		let storyboard = NSStoryboard(name: name, bundle: nil)
+		return storyboard.instantiateInitialController()! as! NSWindowController
+	}
+
+}
---
+++ Frontier/Preferences/DefaultsKey.swift
@@ -0,0 +1,14 @@
+//
+//  DefaultsKey.swift
+//  Frontier
+//
+//  Created by Brent Simmons on 4/19/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultsKey {
+	
+	static let launchOpenODB = "launchOpenODB"
+}
