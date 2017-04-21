3e7bd2e5049030d342cf6034254659889820fd55
Brent Simmons <brent@ranchero.com>
2017-04-20 19:18:29 -0700

    Start FrontierData.framework.

---
+++ FrontierData/FrontierData/FrontierData.h
@@ -0,0 +1,19 @@
+//
+//  FrontierData.h
+//  FrontierData
+//
+//  Created by Brent Simmons on 4/20/17.
+//  Copyright © 2017 Ranchero Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FrontierData.
+FOUNDATION_EXPORT double FrontierDataVersionNumber;
+
+//! Project version string for FrontierData.
+FOUNDATION_EXPORT const unsigned char FrontierDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrontierData/PublicHeader.h>
+
+
